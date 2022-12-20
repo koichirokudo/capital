76ce24a3d69df66d40345379715ce099c8ed3efa
koichirokudo <koichirokudo746@gmail.com>
2022-12-20 23:07:50 +0900

    Headerの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Header from 'components/Header'
 import { Typography } from '@mui/material'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Header />
       <Typography>Hello, Next.js and MUI</Typography>
     </>
   )
