18a5c8c278d505692691e65067c2b7b5c11384a7
koichirokudo <koichirokudo746@gmail.com>
2022-12-26 17:48:56 +0900

    スピナー作成

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,31 @@
+import { alpha, CircularProgress, styled } from '@mui/material'
+import { useSpinnerContext } from 'contexts/SpinnerContext'
+
+const SpinnerWrapper = styled('div')({
+  position: 'fixed',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  backgroundColor: `rgba(255, 255, 255, 0.7)`,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  zIndex: 1200,
+})
+
+const Spinner = () => {
+  const isSpinnerOn = useSpinnerContext()
+
+  return (
+    <>
+      {isSpinnerOn && (
+        <SpinnerWrapper>
+          <CircularProgress size={100} />
+        </SpinnerWrapper>
+      )}
+    </>
+  )
+}
+
+export default Spinner
