2b20aacd37185dad21cde941a040f46c2d9c7d59
koichirokudo <koichirokudo746@gmail.com>
2022-12-28 07:38:28 +0900

    認証できない場合はログインページへ遷移する機能を追加

---
+++ src/utils/hook.ts
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router'
+import { useAuthContext } from 'contexts/AuthContext'
+import { useEffect } from 'react'
+
+export const useAuthGaurd = (): void => {
+  const router = useRouter()
+  const { authUser, isLoading } = useAuthContext()
+
+  useEffect(() => {
+    // ユーザ情報が取得できない場合はログインページに遷移
+    if (!authUser && !isLoading) {
+      const currentPath = router.pathname
+
+      router.push({
+        pathname: '/login',
+        query: {
+          redirect_to: currentPath,
+        },
+      })
+    }
+  }, [router, authUser, isLoading])
+}
