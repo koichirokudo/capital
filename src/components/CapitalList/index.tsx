e736922dc0450d91e6e8d2fa1ad3bbed5339823c
koichirokudo <koichirokudo746@gmail.com>
2023-01-09 19:27:58 +0900

    収支一覧コンポーネントを作成

---
+++ src/components/CapitalList/index.tsx
@@ -0,0 +1,371 @@
+import * as React from 'react'
+import {
+  Alert,
+  AlertProps,
+  Box,
+  Paper,
+  Select,
+  SelectChangeEvent,
+  Snackbar,
+  Typography,
+} from '@mui/material'
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColDef,
+  GridColumns,
+  GridEventListener,
+  GridRenderCellParams,
+  GridRowId,
+  GridRowModel,
+  GridRowModes,
+  GridRowModesModel,
+  GridRowParams,
+  GridToolbar,
+  jaJP,
+  MuiEvent,
+  useGridApiContext,
+} from '@mui/x-data-grid'
+import { Cancel, Delete, Edit, Save } from '@mui/icons-material'
+import updateCapital from 'services/capitals/update-capital'
+import process from 'process'
+import { ApiContext } from 'types'
+import { useSpinnerActionsContext } from 'contexts/SpinnerContext'
+import deleteCapital from 'services/capitals/delete-capital'
+
+/**
+ * 収支一覧をDatagridで表示
+ */
+const CapitalList = ({ capitals }: any) => {
+  const context: ApiContext = {
+    apiRootUrl: process.env.NEXT_PUBLIC_API_BASE_PATH || '/api/proxy',
+  }
+
+  const setSpinner = useSpinnerActionsContext()
+
+  // 行のモードを制御する変数（表示モード/編集モード）
+  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>(
+    {},
+  )
+
+  const [snackbar, setSnackbar] =
+    React.useState<Pick<AlertProps, 'children' | 'severity'> | null>(null)
+
+  /**
+   * 収支タイプをDataGridのセル内で表示する
+   * @param props
+   * @returns
+   */
+  const SelectCategoryTypeEditInputCell = (props: GridRenderCellParams) => {
+    const { id, value, field } = props
+    const apiRef = useGridApiContext()
+
+    const handleChange = async (event: SelectChangeEvent) => {
+      await apiRef.current.setEditCellValue({
+        id,
+        field,
+        value: event.target.value,
+      })
+    }
+
+    return (
+      <Select
+        value={value}
+        onChange={handleChange}
+        size="small"
+        sx={{ height: 1 }}
+        native
+        autoFocus
+      >
+        <option>収入</option>
+        <option>支出</option>
+      </Select>
+    )
+  }
+
+  /**
+   * SelectCategoryTypeEditInputCellのラッパー関数
+   * @param params
+   * @returns
+   */
+  const renderSelectCategoryTypeEditInputCell: GridColDef['renderCell'] = (
+    params,
+  ) => {
+    return <SelectCategoryTypeEditInputCell {...params} />
+  }
+
+  /**
+   * カテゴリをDataGridのセル内で表示する
+   * @param props
+   * @returns
+   */
+  const SelectCategoryEditInputCell = (props: GridRenderCellParams) => {
+    const { id, value, field } = props
+    const apiRef = useGridApiContext()
+
+    const handleChange = async (event: SelectChangeEvent) => {
+      await apiRef.current.setEditCellValue({
+        id,
+        field,
+        value: event.target.value,
+      })
+    }
+
+    return (
+      <Select
+        value={value}
+        onChange={handleChange}
+        size="small"
+        sx={{ height: 1 }}
+        native
+        autoFocus
+      >
+        <option>給料</option>
+        <option>賞与</option>
+        <option>副業</option>
+        <option>お小遣い</option>
+        <option>投資</option>
+        <option>臨時収入</option>
+        <option>日用品</option>
+        <option>教育費</option>
+        <option>美容</option>
+        <option>交通費</option>
+        <option>保険料</option>
+        <option>衣服</option>
+        <option>食費</option>
+        <option>医療費</option>
+        <option>住居費</option>
+        <option>水道</option>
+        <option>ガス</option>
+        <option>電気</option>
+        <option>ローン返済</option>
+        <option>インターネット</option>
+        <option>スマートフォン</option>
+        <option>娯楽費</option>
+        <option>交際費</option>
+        <option>贈答費</option>
+        <option>手数料</option>
+      </Select>
+    )
+  }
+
+  /**
+   * SelectCategoryEditInputCellのラッパー関数
+   * @param params
+   * @returns
+   */
+  const renderSelectCategoryEditInputCell: GridColDef['renderCell'] = (
+    params,
+  ) => {
+    return <SelectCategoryEditInputCell {...params} />
+  }
+
+  // 行編集時にMUIイベントハンドラーのデフォルト動作を無効にする
+  const handleRowEditStart = (
+    params: GridRowParams,
+    event: MuiEvent<React.SyntheticEvent>,
+  ) => {
+    event.defaultMuiPrevented = true
+  }
+
+  // 行編集終了時にMUIイベントハンドラーのデフォルト動作を無効にする
+  const handleRowEditStop: GridEventListener<'rowEditStop'> = (
+    params,
+    event,
+  ) => {
+    event.defaultMuiPrevented = true
+  }
+
+  // 編集アイコンをクリックした行を表示モードから編集モードへ切り替える
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } })
+  }
+
+  // 保存アイコンをクリックした行を編集モードから表示モードに切り替える
+  const handleSaveClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } })
+  }
+
+  // 収支情報を削除するイベント
+  const handleDeleteClick = (id: GridRowId) => async () => {
+    try {
+      setSpinner(true)
+      await deleteCapital(context, id)
+      setSnackbar({
+        children: '収支情報の削除に成功しました。',
+        severity: 'success',
+      })
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setSnackbar({
+          children:
+            '収支情報の削除に失敗しました。もう一度やり直してください。',
+          severity: 'error',
+        })
+      }
+    } finally {
+      setSpinner(false)
+    }
+  }
+
+  // 編集キャンセル
+  const handleCancelClick = (id: GridRowId) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    })
+  }
+
+  const handleCloseSnackbar = () => setSnackbar(null)
+
+  // handleSaveClick実行後に、実行される
+  // 更新前に呼び出される関数
+  const processRowUpdate = React.useCallback(async (newRow: GridRowModel) => {
+    setSpinner(true)
+    const res = await updateCapital(context, newRow)
+    setSpinner(false)
+    setSnackbar({ children: '編集内容を保存しました。', severity: 'success' })
+    return res
+  }, [])
+
+  // processRowUpdateで更新失敗した場合に呼び出される関数
+  const handleProcessRowUpdateError = React.useCallback(() => {
+    setSpinner(false)
+    setSnackbar({
+      children: '編集内容の保存に失敗しました。もう一度やり直してください。',
+      severity: 'error',
+    })
+  }, [])
+
+  const columns: GridColumns = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      width: 110,
+    },
+    {
+      field: 'date',
+      headerName: '日時',
+      width: 150,
+      type: 'date',
+      editable: true,
+    },
+    { field: 'userName', headerName: '更新者', width: 200, editable: true },
+    {
+      field: 'categoryType',
+      headerName: '収支',
+      width: 80,
+      editable: true,
+      renderEditCell: renderSelectCategoryTypeEditInputCell,
+    },
+    {
+      field: 'category',
+      headerName: 'カテゴリ',
+      width: 150,
+      editable: true,
+      renderEditCell: renderSelectCategoryEditInputCell,
+    },
+    {
+      field: 'money',
+      headerName: '金額',
+      width: 120,
+      type: 'number',
+      editable: true,
+    },
+    { field: 'note', headerName: 'メモ', editable: true, width: 300 },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'アクション',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<Save />}
+              label="Save"
+              onClick={handleSaveClick(id)}
+            />,
+            <GridActionsCellItem
+              icon={<Cancel />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+            />,
+          ]
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<Edit />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<Delete />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ]
+      },
+    },
+  ]
+
+  return (
+    <Paper sx={{ p: 2 }}>
+      <Typography variant="h5" sx={{ mb: 1 }}>
+        収支一覧
+      </Typography>
+      <Box sx={{ height: '700px', width: '100%' }}>
+        <DataGrid
+          rows={capitals}
+          getRowId={(row) => row.id}
+          columns={columns}
+          editMode="row"
+          rowHeight={30}
+          pageSize={20}
+          rowsPerPageOptions={[20, 40, 100]}
+          pagination
+          localeText={jaJP.components.MuiDataGrid.defaultProps.localeText}
+          rowModesModel={rowModesModel}
+          onRowModesModelChange={(newModel) => setRowModesModel(newModel)}
+          onRowEditStart={handleRowEditStart}
+          onRowEditStop={handleRowEditStop}
+          processRowUpdate={processRowUpdate}
+          onProcessRowUpdateError={handleProcessRowUpdateError}
+          components={{ Toolbar: GridToolbar }}
+          experimentalFeatures={{ newEditingApi: true }}
+          initialState={{
+            sorting: {
+              sortModel: [
+                {
+                  field: 'date',
+                  sort: 'desc',
+                },
+              ],
+            },
+          }}
+        />
+        {!!snackbar && (
+          <Snackbar
+            open
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            onClose={handleCloseSnackbar}
+            autoHideDuration={6000}
+          >
+            <Alert {...snackbar} onClose={handleCloseSnackbar} />
+          </Snackbar>
+        )}
+      </Box>
+    </Paper>
+  )
+}
+
+export default CapitalList
--- src/components/CategoryList/index.ts
+++ src/components/CategoryList/index.ts
@@ -1,82 +1,106 @@
-export const Categories = () => {
-  return [
-    {
-      category: '未設定',
-    },
-    {
-      category: '給料',
-    },
-    {
-      category: '賞与',
-    },
-    {
-      category: '副業',
-    },
-    {
-      category: 'お小遣い',
-    },
-    {
-      category: '投資',
-    },
-    {
-      category: '臨時収入',
-    },
-    {
-      category: '日用品',
-    },
-    {
-      category: '教育費',
-    },
-    {
-      category: '美容',
-    },
-    {
-      category: '交通費',
-    },
-    {
-      category: '保険料',
-    },
-    {
-      category: '衣服',
-    },
-    {
-      category: '食費',
-    },
-    {
-      category: '医療費',
-    },
-    {
-      category: '住居費',
-    },
-    {
-      category: '水道',
-    },
-    {
-      category: 'ガス',
-    },
-    {
-      category: '電気',
-    },
-    {
-      category: 'ローン返済',
-    },
-    {
-      category: 'インターネット',
-    },
-    {
-      category: 'スマートフォン',
-    },
-    {
-      category: '娯楽費',
-    },
-    {
-      category: '交際費',
-    },
-    {
-      category: '贈答費',
-    },
-    {
-      category: '手数料',
-    },
-  ]
-}
+export const Categories = [
+  {
+    category: '未設定',
+    categoryType: 'その他',
+  },
+  {
+    category: '給料',
+    categoryType: '収入',
+  },
+  {
+    category: '賞与',
+    categoryType: '収入',
+  },
+  {
+    category: '副業',
+    categoryType: '収入',
+  },
+  {
+    category: 'お小遣い',
+    categoryType: '収入',
+  },
+  {
+    category: '投資',
+    categoryType: '収入',
+  },
+  {
+    category: '臨時収入',
+    categoryType: '収入',
+  },
+  {
+    category: '日用品',
+    categoryType: '支出',
+  },
+  {
+    category: '教育費',
+    categoryType: '支出',
+  },
+  {
+    category: '美容',
+    categoryType: '支出',
+  },
+  {
+    category: '交通費',
+    categoryType: '支出',
+  },
+  {
+    category: '保険料',
+    categoryType: '支出',
+  },
+  {
+    category: '衣服',
+    categoryType: '支出',
+  },
+  {
+    category: '食費',
+    categoryType: '支出',
+  },
+  {
+    category: '医療費',
+    categoryType: '支出',
+  },
+  {
+    category: '住居費',
+    categoryType: '支出',
+  },
+  {
+    category: '水道',
+    categoryType: '支出',
+  },
+  {
+    category: 'ガス',
+    categoryType: '支出',
+  },
+  {
+    category: '電気',
+    categoryType: '支出',
+  },
+  {
+    category: 'ローン返済',
+    categoryType: '支出',
+  },
+  {
+    category: 'インターネット',
+    categoryType: '支出',
+  },
+  {
+    category: 'スマートフォン',
+    categoryType: '支出',
+  },
+  {
+    category: '娯楽費',
+    categoryType: '支出',
+  },
+  {
+    category: '交際費',
+    categoryType: '支出',
+  },
+  {
+    category: '贈答費',
+    categoryType: '支出',
+  },
+  {
+    category: '手数料',
+    categoryType: '支出',
+  },
+]
