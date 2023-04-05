d0f3096e4a5c35b3b2d5835f99375a43757353d7
Koichiro Kudo <koichirokudo746@gmail.com>
2023-04-05 17:56:16 +0900

    CapitalFormのテストコードを追加

--- src/components/CapitalForm/index.spec.tsx
+++ src/components/CapitalForm/index.spec.tsx
@@ -14,7 +14,7 @@ describe('CapitalForm', () => {
       <CapitalForm onCapitalSave={onCapitalSave} />,
     )
 
-    expect(getByLabelText(/収支タイプ/i)).toBeInTheDocument()
+    expect(getByText(/収支タイプ/i)).toBeInTheDocument()
     expect(getByLabelText(/日時/i)).toBeInTheDocument()
     expect(getByLabelText(/カテゴリ/i)).toBeInTheDocument()
     expect(getByLabelText(/金額/i)).toBeInTheDocument()
@@ -22,12 +22,12 @@ describe('CapitalForm', () => {
     expect(getByText(/登録/i)).toBeInTheDocument()
   })
 
-  it('テストデータを使用してフォームの送信をする', async () => {
+  it('テストデータを使用してフォームの送信すると onCapitalSaveが呼ばれる', async () => {
     const { getByLabelText, getByText, getByTestId } = render(
       <CapitalForm onCapitalSave={onCapitalSave} />,
     )
 
-    fireEvent.click(getByLabelText(/収支/i))
+    fireEvent.click(getByText(/収支タイプ/i))
     fireEvent.change(getByLabelText(/日時/i), {
       target: { value: '2023/02/02' },
     })
@@ -45,9 +45,24 @@ describe('CapitalForm', () => {
 
     fireEvent.click(getByText(/登録/i))
 
-    // onCapitalSaveが1回呼ばれ、testDataが渡されることを確認する
+    // onCapitalSaveが1回呼ばれること
     await waitFor(() => {
       expect(onCapitalSave).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  it('空の必須フィールドエラーメッセージを表示する', async () => {
+    const { getByText, getByLabelText } = render(
+      <CapitalForm onCapitalSave={onCapitalSave} />,
+    )
+
+    fireEvent.change(getByLabelText('日時'), { target: { value: '' } })
+    fireEvent.change(getByLabelText('金額'), { target: { value: '' } })
+    fireEvent.click(getByText(/登録/i))
+
+    await waitFor(() => {
+      expect(getByText(/日時を選択してください。/i)).toBeInTheDocument()
+      expect(getByText(/金額を入力してください。/i)).toBeInTheDocument()
     })
   })
 })
