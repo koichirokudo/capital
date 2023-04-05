e9f5dd5e7756c393e73f41a4cec73f2e46d3c1d4
Koichiro Kudo <koichirokudo746@gmail.com>
2023-04-05 22:52:30 +0900

    不要変数削除

--- src/components/CapitalList/index.spec.tsx
+++ src/components/CapitalList/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import CapitalList from './index'
 
