55844904abf700d87d9bc5444862d6f8b51716d6
Koichiro Kudo <koichirokudo746@gmail.com>
2023-04-24 20:20:27 +0900

    apiコール関数を直接fetchで実行する

--- src/services/auth/csrf.ts
+++ src/services/auth/csrf.ts
@@ -1,18 +1,14 @@
 import { ApiContext } from 'types'
-import { fetcher } from 'utils'
 
 /**
  * CSRFトークン取得APIコール(Laravel Sanctum)
  * @param context APIコンテキスト
  */
 const getCsrfToken = async (context: ApiContext) => {
-  return await fetcher(
-    `${context.apiRootUrl.replace(/\/$/g, '')}/csrf-cookie`,
-    {
-      method: 'GET',
-      credentials: 'include',
-    },
-  )
+  return await fetch(`${context.apiRootUrl.replace(/\/$/g, '')}/csrf-cookie`, {
+    method: 'GET',
+    credentials: 'include',
+  })
 }
 
 export default getCsrfToken
